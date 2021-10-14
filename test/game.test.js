524492cc1901344d17a2556a67d32b645ce74560
alanbenju <benjualan@bitbucket.org>
2021-10-13 21:10:57 -0300

    Added more tests

--- test/game.test.js
+++ test/game.test.js
@@ -103,7 +103,7 @@ describe("RockPaperScissors", function () {
     ).to.equal(0);
   });
 
-  it("Player 1 creates game with bet and loose with paper against scissors", async function () {
+  it("Player 1 creates game with bet and lose with paper against scissors", async function () {
     const bet = ethers.utils.parseEther("1");
     const prize = ethers.utils.parseEther("2");
 
@@ -163,4 +163,88 @@ describe("RockPaperScissors", function () {
     // Game not created
     expect(await RPSContract.gamesCounter()).to.equal(0);
   });
+
+  it("Tie and both players claim their bets", async function () {
+    const bet = ethers.utils.parseEther("1");
+
+    // Player 1 approve token to bet
+    await token.connect(player1).approve(RPSContract.address, bet);
+
+    // Player 1 creates game
+    await RPSContract.connect(player1).createGame(
+      token.address,
+      player2.address,
+      bet,
+      2,
+      false
+    );
+
+    // Game created
+    expect(await RPSContract.gamesCounter()).to.equal(1);
+
+    // Player 2 approve token to bet
+    await token.connect(player2).approve(RPSContract.address, bet);
+
+    // Player 2 play
+    const gameId = 0;
+    const tx = await RPSContract.connect(player2).play(gameId, 2, false);
+    tx.wait();
+
+    // Game state is finished
+    const game = await RPSContract.games(gameId);
+    expect(game.state).to.equal(1);
+
+    // Player balances are correct
+    expect(
+      await RPSContract.playerTokenBalances(player1.address, token.address)
+    ).to.equal(bet);
+    expect(
+      await RPSContract.playerTokenBalances(player2.address, token.address)
+    ).to.equal(bet);
+
+    expect(await token.balanceOf(player1.address)).to.equal(ethers.utils.parseEther("9"))
+    expect(await token.balanceOf(player2.address)).to.equal(ethers.utils.parseEther("9"))
+    await RPSContract.connect(player1).claimRewards(token.address)
+    await RPSContract.connect(player2).claimRewards(token.address)
+    expect(await token.balanceOf(player1.address)).to.equal(ethers.utils.parseEther("10"))
+    expect(await token.balanceOf(player2.address)).to.equal(ethers.utils.parseEther("10"))
+
+  });
+
+  it.only("Player 1 wins and claims balance. Player 2 can't claim", async function () {
+    const bet = ethers.utils.parseEther("1");
+    const prize = ethers.utils.parseEther("2");
+
+    // Player 1 approve token to bet
+    await token.connect(player1).approve(RPSContract.address, bet);
+
+    // Player 1 creates game
+    await RPSContract.connect(player1).createGame(
+      token.address,
+      player2.address,
+      bet,
+      2,
+      false
+    );
+
+    // Game created
+    expect(await RPSContract.gamesCounter()).to.equal(1);
+
+    // Player 2 approve token to bet
+    await token.connect(player2).approve(RPSContract.address, bet);
+
+    // Player 2 play
+    const gameId = 0;
+    const tx = await RPSContract.connect(player2).play(gameId, 1, false);
+    tx.wait();
+
+    await RPSContract.connect(player1).claimRewards(token.address)
+    //await RPSContract.connect(player2).claimRewards(token.address)
+    expect(await token.balanceOf(player1.address)).to.equal(ethers.utils.parseEther("11"))
+    expect(await token.balanceOf(player2.address)).to.equal(ethers.utils.parseEther("9"))
+
+    const claim = RPSContract.connect(player2).claimRewards(token.address);
+    await expect(claim).to.be.reverted
+  })
+
 });
